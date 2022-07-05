498ccbe739255f9f92693515fb1cf6755c7c3e78
yoniberplay <yoniber.encarnacion@gmail.com>
2022-07-05 12:06:55 -0400

    Hasta aqui yoniber, inicia Christopher.

--- Menu.py
+++ Menu.py
@@ -44,16 +44,16 @@ class Principal(Frame):
         Label(self,text="Seleccione el tipo de integral a resolver",font=("arial",14,BOLD),background="#EBEFFA").place(x=150,y=20)
         ##Label(self,text="Seleccione su integral:",font=("verdana",12,BOLD,ITALIC),background="#f2968a").place(x=250,y=310)
         
-        self.B7=tkinter.Button(self,text="Indefinida",padx=30,pady=10,bg="#33B4F1",width=20,
-        font=("verdana",8,BOLD,ITALIC),command=self.Close1)
+        self.B7=tkinter.Button(self,text="Indefinida",padx=30,pady=10,bg="#33B4F1",fg="#EBEFFA",width=20,
+        font=("verdana",8,BOLD),command=self.Close1)
         self.B7.place(x=215,y=420)
         
-        self.B7=tkinter.Button(self,text="Definida",bg="#33B4F1",width=20,
-        font=("verdana",8,BOLD,ITALIC),padx=30,pady=10,command=self.Close2)
+        self.B7=tkinter.Button(self,text="Definida",bg="#33B4F1",fg="#EBEFFA",width=20,
+        font=("verdana",8,BOLD),padx=30,pady=10,command=self.Close2)
         self.B7.place(x=215,y=370)
         
         self.B7=tkinter.Button(self,text="Longitud de arco",width=20,
-        font=("verdana",8,BOLD,ITALIC),padx=30,pady=10,bg="#33B4F1",command=self.Close3)
+        font=("verdana",8,BOLD),padx=30,pady=10,bg="#33B4F1",fg="#EBEFFA",command=self.Close3)
         self.B7.place(x=215,y=320)
         
 
