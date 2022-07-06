2656cb0726feac0dde5bedc64100beacc720e8a3
yencarnacion07 <yoniber.encarnacion@gmail.com>
2022-07-06 12:41:45 -0400

    Se ha agregado ver documentacion y desarrolladores

--- Menu.py
+++ Menu.py
@@ -1,9 +1,9 @@
+import webbrowser
 from os import close
 from tkinter import *
 import tkinter
+import webbrowser
 from tkinter.font import BOLD, ITALIC, families
-from matplotlib.pyplot import winter
-from sympy.utilities.iterables import rotations
 from ClassLibrary.indefinida import AppIndefinida
 from ClassLibrary.definida import AppDefinida
 from ClassLibrary.longitudArco import LongArco
@@ -40,6 +40,13 @@ class Principal(Frame):
         root.iconbitmap("./Img/portadalibro.ico")
         root.resizable(0,0)
         root.mainloop()
+
+    def verdocumentacion(self):
+        webbrowser.open_new("https://github.com/christopherjael")
+
+    def verdesarrolladores(self):
+        webbrowser.open_new("https://github.com/yoniberplay")
+
     def createWidget(self):
         Label(self,text="Seleccione el tipo de integral a resolver",font=("arial",14,BOLD),background="#EBEFFA").place(x=150,y=20)
         ##Label(self,text="Seleccione su integral:",font=("verdana",12,BOLD,ITALIC),background="#f2968a").place(x=250,y=310)
@@ -55,7 +62,13 @@ class Principal(Frame):
         self.B7=tkinter.Button(self,text="Longitud de arco",width=20,
         font=("verdana",8,BOLD),padx=30,pady=10,bg="#33B4F1",fg="#EBEFFA",command=self.Close3)
         self.B7.place(x=215,y=320)
-        
+
+        self.B7 = tkinter.Button(self, text="Manual de usuario",
+        font=("times", 10, "underline"), bg="#EBEFFA",bd=0,cursor="hand2",command=self.verdocumentacion)
+        self.B7.place(x=10, y=470)
+
+        self.B7 = tkinter.Button(self, text="Developed by",font=("times", 10, "underline"), bg="#EBEFFA", bd=0, cursor="hand2",command=self.verdesarrolladores)
+        self.B7.place(x=550, y=470)
 
 root=Tk()
 app=Principal(root)
@@ -63,12 +76,8 @@ root.iconbitmap("./Img/portadalibro.ico")
 root.title("Calculadora de integrales")
 ima1=ImageTk.PhotoImage(Image.open("./Img/portadalibro.ico"))
 Label(root,image=ima1,bg="#EBEFFA").place(x=225,y=55)
-Label(root,text="Yoniber Encarnacion 2021-1442 || Brian Peña 2021-0948 || Christopher Montero 2021-0622 || Charles Alexandre Meance 2021-9503",bg="#EBEFFA",font=("arial",8,BOLD)).place(x=10,y=470)
 
-##ima2=ImageTk.PhotoImage(Image.open("./Img/diseño1.ico"))
-##Label(root,image=ima2,bg="#f2968a").place(x=400,y=100)
-
-root.eval( 'tk::PlaceWindow . center') #Esto es un codigo ..jjaaj...para centrar la ventana..
+root.eval( 'tk::PlaceWindow . center')
 root.resizable(0,0)
 app.mainloop()
 
